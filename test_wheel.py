b393a2a6459f1e0f3a5a2afcb3926782dcc9b2b7
Brigliano Clément <clement.brigliano@he-arc.ch>
2023-03-28 11:18:02 +0200

    push push push

--- test_wheel.py
+++ test_wheel.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     my_wallet = Wallet(100)
     my_wallet.add_cash(120)
     print("Congratulations. You successfully installed the TP2 package !")
-    print("push")
+    print("pushing to github")
