f071cfe67d724774bc027758250f2f0819930019
Brigliano Clément <clement.brigliano@he-arc.ch>
2023-04-04 11:25:19 +0200

    ap

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="TP2",
-    version="0.2.0",
+    version="0.3.0",
     description="Travail pratique sur la qualité de code et le CI/CD",
     # long_description=open("README.md", encoding="utf-8").read(),
     author="clement.brigliano",
